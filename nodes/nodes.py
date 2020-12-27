7394fe8341a6872bb30786cf6ce66e668de94ca3
Mohammed <mohammedabdalla38@yahoo.com>
2020-12-27 16:20:11 -0600

    node instantiation

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -19,3 +19,17 @@ class Node():
 
   def get_link_node(self):
     return self.link_node
+
+    """
+    Nodes Python Setter
+We are only allowing the value of the node to be set upon creation. However, we want to allow updating the link of the node.
+ For this, we will use a setter to modify the self.link_node attribute.
+
+The method should be called .set_link_node() and should take link_node as an argument. It should then update the self.link_node attribute as appropriate.
+"""
+
+# Define your set_link_node method below:
+
+  def set_link_node(self, link_node):
+     self.link_node = link_node
+     
